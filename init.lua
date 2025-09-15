0455e8ad3c8fe22b1a3d4af75dd1571a47182605
Oskar Arensmeier <oskar.arensmeier@outlook.com>
2025-09-15 07:50:28 +0200

    Some keymaps and colorschema

--- init.lua
+++ init.lua
@@ -1,2 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.o.list = true
+vim.o.listchars = "space:·,tab:-> "
+
+-- Size of an indent
+vim.opt.shiftwidth = 4
+
+-- Number of spaces tabs count for
+vim.opt.tabstop = 4
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,25 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "regex",
+        "vim",
+        "yaml",
+        "rust",
+        "ron",
+      },
+    },
+  },
+}
